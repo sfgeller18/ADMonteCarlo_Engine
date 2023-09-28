2c88abd9155717bc1e53d8503b6e8b485eb4ad3f
sfgeller18 <122637296+sfgeller18@users.noreply.github.com>
2023-09-28 13:41:19 -0700

    Update duals.hpp
    Some opt

--- include/duals.hpp
+++ include/duals.hpp
@@ -36,86 +36,79 @@ struct DualNumber {
         dual = _dual;
     }
 
-    // Overloaded operators for basic arithmetic
-    DualNumber<T> operator+(const DualNumber<T>& other) const {
+   
+    constexpr DualNumber<T> operator+(const DualNumber<T>& other) const noexcept {
         return DualNumber<T>(real + other.real, dual + other.dual);
     }
 
-    DualNumber<T> operator-(const DualNumber<T>& other) const {
+    constexpr DualNumber<T> operator-(const DualNumber<T>& other) const noexcept {
         return DualNumber<T>(real - other.real, dual - other.dual);
     }
 
-    DualNumber<T> operator*(const DualNumber<T>& other) const {
+    constexpr DualNumber<T> operator*(const DualNumber<T>& other) const noexcept {
         return DualNumber<T>(real * other.real, real * other.dual + dual * other.real);
     }
-    DualNumber<T> operator*(const T& other) const {
+
+    constexpr DualNumber<T> operator*(const T& other) const noexcept {
         return DualNumber<T>(real * other, dual * other);
     }
 
-    DualNumber<T>& operator=(const DualNumber<T>& other) {
+    constexpr DualNumber<T>& operator=(const DualNumber<T>& other) noexcept {
         if (this == &other) {
             return *this; // Handle self-assignment
         }
         real = other.real;
         dual = other.dual;
-
         return *this;
     }
 
-       DualNumber<T> operator/(const T& other) const {
-        return DualNumber<T>(real/other, dual/other);
+    constexpr DualNumber<T> operator/(const T& other) const noexcept {
+        return DualNumber<T>(real / other, dual / other);
     }
 
     // Print function
-    void print() const {
+    constexpr void print() const noexcept {
         std::cout << "Real: " << real << std::endl;
         std::cout << "Dual: " << dual << std::endl;
     }
 
     // Additional mathematical functions for DualNumber
-    static DualNumber<T> exp(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> exp(const DualNumber<T>& x) noexcept {
         return DualNumber<T>(std::exp(x.getReal()), x.getDual() * std::exp(x.getReal()));
     }
 
-    static DualNumber<T> log(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> log(const DualNumber<T>& x) noexcept {
         return DualNumber<T>(std::log(x.getReal()), x.getDual() / x.getReal());
     }
 
-    static DualNumber<T> pow(const DualNumber<T>& x, const T& y) {
+    static constexpr DualNumber<T> pow(const DualNumber<T>& x, const T& y) noexcept {
         return DualNumber<T>(std::pow(x.real, y), y * std::pow(x.real, y - 1) * x.dual);
     }
 
-    static DualNumber<T> cos(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> cos(const DualNumber<T>& x) noexcept {
         return DualNumber<T>(std::cos(x.getReal()), -std::sin(x.getReal()) * x.getDual());
     }
 
-    static DualNumber<T> sin(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> sin(const DualNumber<T>& x) noexcept {
         return DualNumber<T>(std::sin(x.getReal()), std::cos(x.getReal()) * x.getDual());
     }
 
-    static DualNumber<T> tan(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> tan(const DualNumber<T>& x) noexcept {
         T cos_x = std::cos(x.getReal());
         return DualNumber<T>(std::tan(x.getReal()), x.getDual() / (cos_x * cos_x));
     }
 
-    static DualNumber<T> acos(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> acos(const DualNumber<T>& x) noexcept {
         return DualNumber<T>(std::acos(x.getReal()), -x.getDual() / std::sqrt(1.0 - x.getReal() * x.getReal()));
     }
 
-    static DualNumber<T> asin(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> asin(const DualNumber<T>& x) noexcept {
         return DualNumber<T>(std::asin(x.getReal()), x.getDual() / std::sqrt(1.0 - x.getReal() * x.getReal()));
     }
 
-    static DualNumber<T> atan(const DualNumber<T>& x) {
+    static constexpr DualNumber<T> atan(const DualNumber<T>& x) noexcept {
         return DualNumber<T>(std::atan(x.getReal()), x.getDual() / (1.0 + x.getReal() * x.getReal()));
     }
 };
-
-
-
-
-
-
-
 
 #endif
